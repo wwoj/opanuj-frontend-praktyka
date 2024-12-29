0242c60cb8f1751836be246e7efdc113e110ab4c
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2024-12-29 16:03:06 +0100

    +prop-drilling-start

--- examples/module1/lesson2/prop-drilling-start/components/SearchForm.tsx
+++ examples/module1/lesson2/prop-drilling-start/components/SearchForm.tsx
@@ -1,30 +1,12 @@
-import GenderSelect from './GenderSelect';
-import { NameField } from './NameField';
-import SortSelect from './SortSelect';
+import React from 'react';
 
 type SearchFormProps = {
-  name: string;
-  setName: React.Dispatch<React.SetStateAction<string>>;
-  gender: string;
-  setGender: React.Dispatch<React.SetStateAction<string>>;
-  sortOption: string;
-  setSortOption: React.Dispatch<React.SetStateAction<string>>;
+  children: React.ReactNode;
 };
 
-function SearchForm({
-  name,
-  setName,
-  gender,
-  setGender,
-  sortOption,
-  setSortOption,
-}: SearchFormProps) {
+function SearchForm({ children }: SearchFormProps) {
   return (
-    <form className="space-x-4 flex items-end justify-center">
-      <NameField name={name} setName={setName} />
-      <GenderSelect gender={gender} setGender={setGender} />
-      <SortSelect sortOption={sortOption} setSortOption={setSortOption} />
-    </form>
+    <form className="space-x-4 flex items-end justify-center">{children}</form>
   );
 }
 
--- examples/module1/lesson2/prop-drilling-start/containers/CharacterSearchContainer.tsx
+++ examples/module1/lesson2/prop-drilling-start/containers/CharacterSearchContainer.tsx
@@ -4,6 +4,9 @@ import SearchForm from '../components/SearchForm';
 import SearchTitle from '../components/SearchTitle';
 import { useCharacterSearch } from '../hooks/useCharacterSearch';
 import { sortCharacters } from '../utils/sortCharacters';
+import { NameField } from '../components/NameField';
+import GenderSelect from '../../container-components/components/GenderSelect';
+import SortSelect from '../components/SortSelect';
 
 function CharacterSearchContainer() {
   const [name, setName] = useState('');
@@ -18,14 +21,11 @@ function CharacterSearchContainer() {
       <div className="pt-20" />
       <SearchTitle />
       <div className="pt-8" />
-      <SearchForm
-        name={name}
-        setName={setName}
-        gender={gender}
-        setGender={setGender}
-        sortOption={sortOption}
-        setSortOption={setSortOption}
-      />
+      <SearchForm>
+        <NameField name={name} setName={setName} />
+        <GenderSelect gender={gender} setGender={setGender} />
+        <SortSelect sortOption={sortOption} setSortOption={setSortOption} />
+      </SearchForm>
       <div className="pt-12" />
       <CharacterList characters={sortedCharacters} />
       <div className="pt-16" />
