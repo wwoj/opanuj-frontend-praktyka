c5dd4aef9079d2a776d3aaaef573452a462f7499
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2025-01-06 17:45:22 +0100

    +lesson-4/interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,18 +1,25 @@
 import axios from 'axios';
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
+const reqisterStartDate = (config) => {
+  config.metadata = { startDate: new Date() };
   return config;
-});
+};
+
+const reqisterEndDate = (response) => {
+  const {
+    config: { url, metadata },
+  } = response;
+  const elapsedTime = new Date() - metadata.startDate;
+  console.log(`Request to ${url} took ${elapsedTime}ms`);
+
+  return response;
+};
+// Add a request interceptor
+axios.interceptors.request.use(reqisterStartDate);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  return response;
-});
+axios.interceptors.response.use(reqisterEndDate);
 
-const {
-  data: { articles },
-} = await axios.get('/api/data/articles?timeout=3000');
+const { data } = await axios.get('/api/data/articles?timeout=3000');
 
-document.querySelector('#data').innerHTML = articles[0].content;
+document.querySelector('#data').innerHTML = data[0].content;
