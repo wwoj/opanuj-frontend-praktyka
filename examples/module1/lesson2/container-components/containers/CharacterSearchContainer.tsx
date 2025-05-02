82b43b81a1a7714b0dfce990e0451374c7e2b419
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2025-05-02 10:32:31 +0200

    +module-1: Lesson-2: rick&morty

--- examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
+++ examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
@@ -3,32 +3,16 @@ import CharacterList from '../components/CharacterList';
 import SearchForm from '../components/SearchForm';
 import SearchTitle from '../components/SearchTitle';
 import type { Character } from '../types/Character';
+import { characterSearch } from '../hooks/characterSearch';
+import { sortCharacters } from '../utils/sortCharacters';
 
 function CharacterSearchContainer() {
   const [name, setName] = useState('');
   const [gender, setGender] = useState('');
-  const [characters, setCharacters] = useState<Character[]>([]);
+  const characters = characterSearch(name, gender);
   const [sortOption, setSortOption] = useState('');
 
-  useEffect(() => {
-    if (name || gender) {
-      fetch(
-        `https://rickandmortyapi.com/api/character/?name=${name}&gender=${gender}`
-      )
-        .then((response) => response.json())
-        .then((data) => setCharacters(data.results || []))
-        .catch((error) => console.error('Error fetching data:', error));
-    }
-  }, [name, gender]);
-
-  const sortedCharacters = [...characters].sort((a, b) => {
-    if (sortOption === 'name') {
-      return a.name.localeCompare(b.name);
-    } else if (sortOption === 'created') {
-      return new Date(a.created).getTime() - new Date(b.created).getTime();
-    }
-    return 0;
-  });
+  const sortedCharacters = sortCharacters(characters, sortOption);
 
   return (
     <>
---
+++ examples/module1/lesson2/container-components/hooks/characterSearch.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { type Character } from '../types/Character';
+
+export function characterSearch(name: string, gender: string) {
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  useEffect(() => {
+    if (name || gender) {
+      if (name || gender) {
+        fetch(
+          `https://rickandmortyapi.com/api/character/?name=${name}&gender=${gender}`
+        )
+          .then((response) => response.json())
+          .then((data) => setCharacters(data.results || []))
+          .catch((error) => console.error('Error fetching data:', error));
+      }
+    }
+  }, [name, gender]);
+
+  return characters;
+}
