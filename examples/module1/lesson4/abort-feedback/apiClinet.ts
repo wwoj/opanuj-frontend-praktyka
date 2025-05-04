46028929d4b217d527a3e3ad6a830b8128b0bd47
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2025-05-04 15:34:03 +0200

    +module-4: Lesson-4: abort-feedback

---
+++ examples/module1/lesson4/abort-feedback/apiClinet.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const API_URL = '/api/data/users?timeout=10000';
+
+interface User {
+  id: number;
+  name: string;
+}
+
+export function apiClient() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isTimeoutError, setIsTimeoutError] = useState(false);
+
+  function fetchUsers() {
+    axios
+      .get<User[]>(API_URL, { timeout: 5000 })
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        if (error.code === 'ECONNABORTED') {
+          setIsTimeoutError(true);
+        }
+      });
+  }
+
+  function retryRequest() {
+    setIsTimeoutError(false);
+    fetchUsers();
+  }
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  return { users, isTimeoutError, retryRequest };
+}
