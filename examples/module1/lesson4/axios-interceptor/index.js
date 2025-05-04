a6aa06d4cf208ce4646f49b60ca27698b9e25d4b
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2025-05-04 15:41:42 +0200

    +module-1: Lesson-4: Axios Interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.metadata = { startTime: new Date() };
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
+  const {
+    config: { url, metadata },
+  } = response;
+  const elapsedTime = new Date() - metadata.startTime;
+  console.log(`Request to ${url} took ${elapsedTime}ms`);
   return response;
 });
 
