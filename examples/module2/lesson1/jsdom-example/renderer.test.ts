df0132c51dff8a56bee0613d99567a5b13013fc6
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2025-05-11 19:27:38 +0200

    +module-2: Lesson-2: jsdom-example

--- examples/module2/lesson1/jsdom-example/renderer.test.ts
+++ examples/module2/lesson1/jsdom-example/renderer.test.ts
@@ -15,7 +15,16 @@ describe('User renderer', () => {
 
     const container = document.createElement('div');
     renderItems(container, users);
-    expect(Array.from(container.querySelectorAll('li'))).toHaveLength(3);
+
+    const items = Array.from(container.querySelectorAll('li'));
+    expect(items).toHaveLength(3);
+
+    expect(items.map((li) => li.textContent)).toEqual([
+      'false Name: John, Age: 30',
+      '(Admin) Name: Jane, Age: 25',
+      'false Name: Jack, Age: 40',
+    ]);
+    // expect(Array.from(container.querySelectorAll('li'))).toHaveLength(3);
   });
 
   test('should render only regular users if non-admin is rendering the list', () => {
@@ -23,6 +32,12 @@ describe('User renderer', () => {
 
     const container = document.createElement('div');
     renderItems(container, users);
-    expect(Array.from(container.querySelectorAll('li'))).toHaveLength(2);
+
+    const items = Array.from(container.querySelectorAll('li'));
+    expect(items).toHaveLength(2);
+    expect(items.map((li) => li.textContent)).toEqual([
+      'false Name: John, Age: 30',
+      'false Name: Jack, Age: 40',
+    ]);
   });
 });
